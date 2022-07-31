d95e7ad4ec6f8e1fac38533ece14b75ca89206cb
AkosKovacs192 <102877635+AkosKovacs192@users.noreply.github.com>
2022-07-31 10:44:20 +0100

    Initial commit

---
+++ MauiPaint/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace MauiPaint;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
